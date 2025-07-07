b1f71c9e0680e07723e9e6b7c0d504af1bd470a6
Daniel <122654917+dtecoder46@users.noreply.github.com>
2025-07-07 12:52:54 +0000

    added a fourth palette

--- color-collection/src/App.vue
+++ color-collection/src/App.vue
@@ -14,6 +14,8 @@ import HelloWorld from "./components/HelloWorld.vue";
 
     <br />
 
+    <h2 class="ml-5 text-3xl">Palette 1</h2>
+
     <button class="bg-ffbf00 p-3 ml-5 border-4">#FFBF00</button>
     <button class="bg-00ff40 p-3 ml-5 border-4">#00FF40</button>
     <button class="bg-0040ff p-3 ml-5 border-4">#0040FF</button>
@@ -24,6 +26,8 @@ import HelloWorld from "./components/HelloWorld.vue";
     <hr class="solid border-4" />
 
     <br />
+
+    <h2 class="ml-5 text-3xl">Palette 2</h2>
 
     <button class="bg-b29a72 p-3 ml-5 border-4">#B29A72</button>
     <button class="bg-222019 p-3 ml-5 border-4 text-white">#222019</button>
@@ -37,11 +41,13 @@ import HelloWorld from "./components/HelloWorld.vue";
 
     <br />
 
-    <button class="bg-353e67 p-3 ml-5 border-4 text-white">#B29A72</button>
-    <button class="bg-ccac95 p-3 ml-5 border-4">#222019</button>
-    <button class="bg-76abd8 p-3 ml-5 border-4">#DFD9CF</button>
-    <button class="bg-532c19 p-3 ml-5 border-4 text-white">#595441</button>
-    <button class="bg-6292c8 p-3 ml-5 border-4">#5F2F17</button>
+    <h2 class="ml-5 text-3xl">Palette 3</h2>
+
+    <button class="bg-353e67 p-3 ml-5 border-4 text-white">#353E67</button>
+    <button class="bg-ccac95 p-3 ml-5 border-4">#CCAC95</button>
+    <button class="bg-76abd8 p-3 ml-5 border-4">#76ABD8</button>
+    <button class="bg-532c19 p-3 ml-5 border-4 text-white">#532C19</button>
+    <button class="bg-6292c8 p-3 ml-5 border-4">#6292C8</button>
 
     <br /><br />
 
@@ -49,10 +55,13 @@ import HelloWorld from "./components/HelloWorld.vue";
 
     <br />
 
-    <button class="bg-ffbf00 p-3 ml-5 border-4">#FFBF00</button>
-    <button class="bg-00ff40 p-3 ml-5 border-4">#00FF40</button>
-    <button class="bg-0040ff p-3 ml-5 border-4">#0040FF</button>
-    <button class="bg-ff00c0 p-3 ml-5 border-4">#FF00C0</button>
+    <h2 class="ml-5 text-3xl">Palette 4</h2>
+
+    <button class="bg-a08267 p-3 ml-5 border-4">#A08267</button>
+    <button class="bg-211f21 p-3 ml-5 border-4 text-white">#211F21</button>
+    <button class="bg-6d2c18 p-3 ml-5 border-4 text-white">#6D2C18</button>
+    <button class="bg-d4d8d6 p-3 ml-5 border-4">#D4D8D6</button>
+    <button class="bg-cdc6b0 p-3 ml-5 border-4">#CDC6B0</button>
   </div>
 </template>
 
